5fdc2552070ffd2a297539b9b83ca195fa5d8d1e
amandeepsingh1929 <as0632568@gmail.com>
2023-01-11 22:15:44 +0530

    Backend Completed . Now frontend setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,61 +1,98 @@
-const asyncHandler= require('express-async-handler')
-const bcrypt=require('bcryptjs');
-const {User}=require('../models/userModel')
-const express=require('express');
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 // @desc Register a new user
 // @route /api/users
 // @acess Public
-const registerUser= asyncHandler(async(req,res)=>{
-  const {name,email,password} = req.body;
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-  if(!name || !email || !password){
-   res.status(400)
-   throw new Error("Please include all fields");
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
   }
-  
+
   //Find if user already exist
 
-  const userExist= await User.findOne({email}).exec();
+  const userExist = await User.findOne({ email });
 
-  if(userExist){
-    res.status(400)
-    throw new Error('User already exists')
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
   }
 
   // Hash password
 
-  const salt=await bcrypt.genSalt(10);
-  const hashedPassword=await bcrypt.hash(password,salt)
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
   //Create user
 
-  const user=await User.create({
+  const user = await User.create({
     name,
     email,
-    password:hashedPassword
-  })
+    password: hashedPassword,
+  });
 
-  if(user){
-    res.status(201),json({
-      _id:user._id,
-      name:user.name,
-      email:user.email
-    })
-  }else{
-    res.status(400)
-    throw new error('Invalid user data')
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
   }
-}
-)
+});
 
 // @desc Login user
 // @route /api/users
 // @acess Public
-const loginUser= asyncHandler(async (req,res)=>{
-  res.send('Login Route');
-}
-)
-module.exports={
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // Check User and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Credentials");
+  }
+});
+
+// @desc Get current user
+// @route /api/users.me
+// @acess Private
+const getMe=asyncHandler(async(req,res)=>{
+  const user={
+    id: req.user._id,
+    email: req.user.email,
+    name:req.user.name
+  }
+  res.status(200).json(user)
+})  
+
+
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = {
   registerUser,
-  loginUser
-} 
+  loginUser,
+  getMe
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
-const {registerUser,loginUser}=require('../controllers/userController')
+const {registerUser,loginUser,getMe}=require('../controllers/userController')
+const {protect}=require('../middleware/authMiddleware')
 
 
 //For register user
@@ -9,5 +10,9 @@ router.post('/',registerUser)
 
 //For login user
 router.post('/login',loginUser)
+
+
+// For protected route
+router.get('/me',protect,getMe)
 
 module.exports=router
